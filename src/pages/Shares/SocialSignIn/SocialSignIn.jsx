5adf019a1b79017bd6e45e30e44bd5fab231da20
saifularafat <saifularafat.info@gmail.com>
2023-06-08 15:43:15 +0600

    social icone to the signIn Done!

--- src/pages/Shares/SocialSignIn/SocialSignIn.jsx
+++ src/pages/Shares/SocialSignIn/SocialSignIn.jsx
@@ -4,7 +4,57 @@ import { useLoaderData, useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 
 const SocialSignIn = () => {
-    
+    const navigate = useNavigate();
+    const location = useLoaderData();
+    const from = location.state?.from?.pathname || "/";
+
+    const { setLoading, googleSignIn, githubSignIn } = useAuth();
+
+    const handlerGoogleLogin = () => {
+        googleSignIn()
+        .then(result => {
+            console.log(result);
+            Swal.fire({
+                position: 'top-end',
+                icon: 'success',
+                title: 'Your SignUp Successful',
+                showConfirmButton: false,
+                timer: 1500
+            })
+            navigate(from, { replace: true })
+        })
+        .catch(error => {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: `${error.message}`,
+            })
+            setLoading(false)
+        })
+    }
+
+    const handlerGitHubLogin = () => {
+        githubSignIn()
+        .then(result => {
+            console.log(result);
+            Swal.fire({
+                position: 'top-end',
+                icon: 'success',
+                title: 'Your SignUp Successful',
+                showConfirmButton: false,
+                timer: 1500
+            })
+            navigate(from, { replace: true })
+        })
+        .catch(error => {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: `${error.message}`,
+            })
+            setLoading(false)
+        })
+    }
 
     return (
         <div>
